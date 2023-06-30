8396c1e7b5914f7809332bc75d7692a12550db54
Somtochukwu Nnaji <lyon4197@gmail.com>
2023-06-30 21:20:09 +0100

    Almost d..

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,12 @@ import { Dashboard } from "./components/Pages/Dashboard/Dashboard";
 const App = () => {
 	return (
 		<div className="font-roboto h-[100vh] w-full">
+
 			<Dashboard />
 		</div>
 	);
 };
 
 export default App;
+
+//You prolly know better where to keep the side bar do me do the honour and fix it 
